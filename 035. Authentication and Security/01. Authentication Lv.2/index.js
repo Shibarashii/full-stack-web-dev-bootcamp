7c9d35d542afe295b6493d03a3fc4af90d36ced1
Seanrei Ethan M. Valdeabella <147302321+Shibarashii@users.noreply.github.com>
2026-09-11 13:02:15 +0800

    finish lvl 2 auth: bcrypt and salting

---
+++ 035. Authentication and Security/01. Authentication Lv.2/index.js
@@ -0,0 +1,101 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+import dotenv from "dotenv";
+import bcrypt from "bcrypt";
+
+dotenv.config({ path: "../../.env" });
+
+const app = express();
+const port = 3000;
+const saltRounds = 10;
+
+const db = new pg.Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: "secrets",
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.post("/register", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
+
+  try {
+    const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+
+    if (checkResult.rows.length > 0) {
+      res.send("Email already exists. Try logging in.");
+    } else {
+      // Password Hashing
+      bcrypt.hash(password, saltRounds, async (err, hash) => {
+        if (err) {
+          console.log(err);
+        }
+        const result = await db.query(
+          "INSERT INTO users (email, password) VALUES ($1, $2)",
+          [email, hash],
+        );
+
+        console.log(result);
+        res.render("secrets.ejs");
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const email = req.body.username;
+  const loginPassword = req.body.password;
+
+  try {
+    const result = await db.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+    if (result.rows.length > 0) {
+      const user = result.rows[0];
+      const storedHashedPassword = user.password;
+
+      bcrypt.compare(loginPassword, storedHashedPassword, (err, result) => {
+        if (err) console.log(err);
+        else {
+          if (result) {
+            console.log(result);
+            res.render("secrets.ejs");
+          } else {
+            res.send("Incorrect password.");
+          }
+        }
+      });
+    } else {
+      res.send("User not found");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
